c512ffbce880009fad1a65e7e522b78a6ae80960
Mike <miguel.suarezf05@gmail.com>
2023-04-03 21:16:54 -0500

    Added useEffect to fetch first api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react"
+
+const API_ENDPOINT_CAT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const API_ENDPOINT_CAT_IMAGE = `https://cataas.com/cat/says/${firstword}?json=true`
+
 export function App () {
+    const [fact, setFact] = useState()
+
+    useEffect(() => {
+      fetch(API_ENDPOINT_CAT_RANDOM_FACT)
+        .then(res => res.json())
+        .then(data => setFact(data.fact))
+    }, [])
+    
     return (
-    <h1>App de gatitos</h1>
+        <main>
+            <h1>App de gatitos</h1>
+            {fact && <p>{fact}</p>}
+        </main>
+    
     )
 }
